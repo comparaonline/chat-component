8c029b36c73c184deef5f3596914c4de2844921c
Ignacio Zabalza <zabalza.ign@gmail.com>
2017-05-30 19:44:52 -0300

    Add minimal ZopimChat component

---
+++ src/ZopimChat.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ZopimChat extends Component {
+  componentDidMount() {
+    console.log('ZopimChat did mount');
+  }
+
+  render() {
+    return (
+      <span>ZopimChat</span>
+    );
+  }
+}
+
+export default ZopimChat;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+
+const name = 'chat-component';
+
+const config = {
+  context: __dirname,
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `${name}.min.js`,
+    library: name,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        query: { cacheDirectory: true }
+      }
+    ]
+  },
+  externals: {
+    react: 'react'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false },
+      screw_ie8: true,
+      sourceMap: false
+    }),
+    new WebpackCleanupPlugin()
+  ]
+};
+
+module.exports = config;
